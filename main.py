4d2ac444b3978b663fd6cdc55ea1c5b0303092e7
daven <davenlu1989@gmail.com>
2023-11-18 14:05:38 +0900

    allow multiple sessions at once

--- main.py
+++ main.py
@@ -13,6 +13,41 @@ from fastapi import (
 app = FastAPI()
 
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
+
+
 async def get_cookie_or_token(
     websocket: WebSocket,
     session: Annotated[str | None, Cookie()] = None,
@@ -23,21 +58,7 @@ async def get_cookie_or_token(
     return session or token
 
 
-@app.websocket("/messaging")
-async def websocket_endpoint(
-    websocket: WebSocket,
-    cookie_or_token: Annotated[str, Depends(get_cookie_or_token)],
-    q: int | None = None,
-):
-    await websocket.accept()
-    print("cookie", cookie_or_token)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            print(f"Received data {data}")
-            print(f"Cookie/query_token {cookie_or_token}")
-            if q is not None:
-                await websocket.send_text(f"Query parameter is:", {q})
-            await websocket.send_text(data)
-    except WebSocketDisconnect:
-        print(f"client disconnected")
+# Sanity check. 127.0.0.1:8000/ should give hello world when accessed via http
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
